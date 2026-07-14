cf95ad8968cfcbc4b77792b1621c6a8111708413
yunilaD <yunilad2006@gmail.com>
2026-07-14 11:04:30 +0530

    feat: initialize Express server with menu retrieval and contact form submission endpoints

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { fileURLToPath } from "url";
 import path from "path";
 
@@ -10,6 +10,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(express.json());
 
 //returns the full menu as Json
 app.get("/api/menu", async (req, res) => {
@@ -24,6 +25,35 @@ app.get("/api/menu", async (req, res) => {
         res.status(500).json({ error: "Can't load manu" });
     }
 });
+
+//recieve data from browser
+app.post("api/contact", async (req, res) => {
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+        return res.status(400).json({ error: "missing fields" });
+    }
+
+    const entry = { name, email, message, recievedAt: new Date().toISOString() };
+
+    const filePath = path.join(__dirname, "messages.json");
+    let messages = [];
+
+    try {
+        const raw = await readFile(filePath, "utf-8");
+        messages = JSON.parse(raw);
+    }
+    catch {
+
+
+    }
+    messages.push(entry);
+    await writeFile(filePath, JSON.stringify(messages, null, 2));
+
+    res.status(201).json({ success: "recieved", entry });
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Menu API running at http://localhost:${PORT}/api/menu`);
